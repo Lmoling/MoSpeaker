ed23047986dfddb8438ea1521007eea9783374f8
lmoling <lmoling@yeah.net>
2017-01-03 17:35:47 +0800

    readme

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Moling on 17/1/3.
+//  Copyright © 2017年 Moling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
